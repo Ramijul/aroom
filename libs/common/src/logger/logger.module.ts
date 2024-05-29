9bb7e74668c017bca00e839630af074b6707bb57
rami <ramizul127@gmail.com>
2024-05-29 09:48:23 -0400

    disable pino-pretty in production

--- libs/common/src/logger/logger.module.ts
+++ libs/common/src/logger/logger.module.ts
@@ -1,15 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
   imports: [
-    PinoLoggerModule.forRoot({
-      pinoHttp: {
-        transport: {
-          target: 'pino-pretty',
-          options: { singleLine: true },
-        },
+    PinoLoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const env: string = configService.get('NODE_ENV') ?? 'dev';
+        // disable pino-pretty in production
+        return {
+          pinoHttp:
+            env.toLowerCase() !== 'production'
+              ? {
+                  transport: {
+                    target: 'pino-pretty',
+                    options: { singleLine: true },
+                  },
+                }
+              : undefined,
+        };
       },
+      inject: [ConfigService],
     }),
   ],
 })
